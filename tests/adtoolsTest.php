cb6369c60b7097dd2a8c1ff30aa0f729f1b5799a
datagutten <datagutten@datagutten.net>
2023-09-29 18:31:46 +0200

    Add more tests

--- tests/adtoolsTest.php
+++ tests/adtoolsTest.php
@@ -12,6 +12,7 @@ use Exception;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use storfollo\adtools;
+use Symfony\Component\Ldap;
 
 
 class adtoolsTest extends TestCase
@@ -51,12 +52,27 @@ class adtoolsTest extends TestCase
         adtools\adtools::connect_config($this->config['missing_dc']);
     }
 
+    public function testInvalidProtocol()
+    {
+        $adtools=new adtools\adtools();
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid protocol specified');
+        $adtools->connect_and_bind('a', 'b', 'b', 'xxx');
+    }
+
     public function testConnect_and_bind_no_username()
     {
         $adtools=new adtools\adtools();
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Username and/or password are not specified');
         $adtools->connect_and_bind('', '', '');
+    }
+
+    public function testConnect_and_bind_ldaps()
+    {
+        $adtools=new adtools\adtools();
+        $this->expectException(Ldap\Exception\ConnectionException::class);
+        $adtools->connect_and_bind('cn=admin,dc=example,dc=com', 'test', 'localhost', 'ldaps');
     }
 
     public function testConnect_and_bind_invalid_chars()
@@ -85,21 +101,31 @@ class adtoolsTest extends TestCase
         $this->adtools->ldap_query('(objectclass=foo)', array('base_dn'=>'OU=Test,DC=example,DC=com'));
     }
 
-/*
-    public function testQuery()
+    public function testFindObject()
     {
+        $user = $this->adtools->find_object('user1', 'OU=Test,DC=example,DC=com');
+        $this->assertInstanceOf(adtools\User::class, $user);
+        $this->assertEquals('cn=user1,ou=Users,ou=adtools-test,ou=Test,dc=example,dc=com', $user->dn);
 
+        $user = $this->adtools->find_object('user1', 'OU=Test,DC=example,DC=com', 'username');
+        $this->assertInstanceOf(adtools\User::class, $user);
+        $this->assertEquals('cn=user1,ou=Users,ou=adtools-test,ou=Test,dc=example,dc=com', $user->dn);
+
+        $this->expectException(adtools\exceptions\NoHitsException::class);
+        $user = $this->adtools->find_object('user2@upn.local', 'OU=Test,DC=example,DC=com', 'upn');
+        /*$this->assertInstanceOf(adtools\User::class, $user);
+        $this->assertEquals('cn=user2,ou=Users,ou=adtools-test,ou=Test,dc=example,dc=com', $user->dn);*/
     }
 
-    public function testExtract_field()
+    public function testObjectNotFound()
     {
-
+        $this->expectException(adtools\exceptions\NoHitsException::class);
+        $this->adtools->find_object('computer1', 'OU=Test,DC=example,DC=com', 'computer');
     }
-*/
 
-/*
-    public function testLogin_form()
+    public function testInvalidObject()
     {
-
-    }*/
+        $this->expectException(InvalidArgumentException::class);
+        $this->adtools->find_object('computer1', 'OU=Test,DC=example,DC=com', 'computer_bad');
+    }
 }
